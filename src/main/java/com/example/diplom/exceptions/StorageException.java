c94c0f84d24deb58aac9632cecd3024b859f1336
Arseny Sazonov <gwentsenya@gmail.com>
2021-12-10 23:38:27 +0500

    Add ITests, minor changes

--- src/main/java/com/example/diplom/exceptions/StorageException.java
+++ src/main/java/com/example/diplom/exceptions/StorageException.java
@@ -1,6 +1,8 @@
 package com.example.diplom.exceptions;
 
-public class StorageException extends RuntimeException{
+import org.springframework.security.core.AuthenticationException;
+
+public class StorageException extends AuthenticationException {
     public StorageException(String message) {
         super(message);
     }
